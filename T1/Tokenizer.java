5e98800c5b20297746c2c0f8fb5150901c5799f3
Sr3284 <daviqzr@gmail.com>
2022-09-05 17:23:37 -0400

    Adiciona todos os tipos de token válidos

--- T1/Tokenizer.java
+++ T1/Tokenizer.java
@@ -7,9 +7,9 @@ public class Tokenizer {
 
 	//Possui três tokens "(", ")" e "atomos"
 	public static enum Ttipo {
-		INTER, BOOL, VERD, FALS, READ, WRITE,						//Predefined identifiers 
+		INTER, BOOL, VERD, FALS, READ, WRITE,						//Predefined identifiers
 		EPAREN, DPAREN, PONTO_VIRG, VIRG, ECHAVES, DCHAVES,			//Pontuação
-		OP_EQUALS, OP_DIFF, OP_LESS, OP_LEQ, OP_GEQ, OP_GREAT, 
+		OP_EQUALS, OP_DIFF, OP_LESS, OP_LEQ, OP_GEQ, OP_GREAT,
 		OP_PLUS, OP_MINUS, OP_OR, OP_MULT, OP_DIV, OP_AND, OP_NOT,	//Simbolos de operações
 		VOID, GOTO, RETURN, IF, WHILE,								//Reservadas?
 		INVALIDO;													//Debug
