4c9cfd21dbffd5fead89d018c5d8db8944de5811
Sr3284 <daviqzr@gmail.com>
2022-11-08 19:22:27 -0400

    Atualiza parser() e verifica()

--- T1/C/parserRec.c
+++ T1/C/parserRec.c
@@ -6,38 +6,27 @@
 #include <stdlib.h>
 #include "tokenizer.h"
 
+#define ID_TAM 15
+
 FILE *f;
 int la;		//lookahead
 
 // Verifica se o próximo terminal é t e avança para o próximo token
 void verifica(int t) {
+	
 	if (la == COMENT)
 		la = tokenizer();
 
 	if (la == ERR) {
-		printf("Erro Léxico(linha: %d): símbolo inválido \"%s\" encontrado.",);
+		printf("Erro Léxico(linha: %d): símbolo inválido \"%s\" encontrado.", linhas, lexema);
 		exit(1);
 	}
 
 	if (la == t) {
 		la = tokenizer();
 	} else {
-		printf("Erro Sintático(linha: %d): token \"%s\" esperado.\"%s\" encontrado.\n",);
+		printf("Erro Sintático(linha: %d): token \"%s\" esperado.\"%s\" encontrado.\n", linhas, terminais[t], lexema);
 		exit(1);
-	}
-}
-
-
-/* Inicia o processamento e devolve mensagem se a texto estiver 
-sintáticamente correto */
-char* parser() {
-	la = tokenizer();
-	S();
-
-	if (la == FIM) {
-		return("Fim do arquivo alcançado, a sintaxe está correta!");
-	} else {
-		return("Fim do arquivo não alcançado.");
 	}
 }
 
@@ -72,13 +61,13 @@ void FPS_() {
 	} else {
 		if(la == VAR || la == VAZIO || la == ID) {
 			FP();
-			FKP();
+			FPK();
 			verifica(DPAREN);
 		}
 	}
 }
 
-void FP() {
+void FP() {		// <- dor de cabeça provavelmente aqui
 	if (la == ID) {
 		verifica(ID);
 
@@ -91,11 +80,59 @@ void FP() {
 		}
 	} else if (la == VAR) {
 		EP();
-	} else	if (la == VAZIO) {}
+	} else	if (la == VAZIO) {
+		FUP();
+	}
+}
+
+void FPK() {
+	if (la == VIRG) {
+		verifica(VIRG);
+		FP();
+		FPK();
+	}
+}
+
+void EP() {
+	if (la == VAR) {
+		EPO();
+	}
+
+
+}
+
+/* Inicia o processamento e devolve mensagem se a texto estiver 
+sintáticamente correto */
+char* parser() {
+
+	la = tokenizer();
+
+	S();
+
+	if (la == FIM) {
+		return("Fim do arquivo alcançado, a sintaxe está correta!");
+	} else {
+		return("Fim do arquivo não alcançado.");
+	}
 }
 
 // Função MAIN
 int int main(int argc, char *argv[]) {
-	/* code */
+
+	if (argc < 2) {
+		printf("Uso: ./parser <arquivo>\n");
+	}
+
+	f = fopen(argv[1], "r");
+
+	if (!f) {
+		printf("Erro na abertura do arquivo!\n");
+		return 1;
+	}
+
+	parser();
+
+	fclose(f);
+
 	return 0;
 }
